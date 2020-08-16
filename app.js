0f2fecc3898e4a418b7ae2086ad3fca57bd4a972
jamieleegtb <cemilegtaman@gmail.com>
2020-08-16 17:42:16 -0500

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
       //Fetching data from JSON file
       fetch("dino.json")
-      .then(response => response.json())
-      .then(json => console.log(JSON.stringify(json)));
+        .then(response => response.json())
+        .then(dino => console.log(JSON.stringify(dino)));
 
 
       // Create Dino Constructor
