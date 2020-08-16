1a63d698ff602b21b353567dcdf93df5b1c2ef95
jamieleegtb <cemilegtaman@gmail.com>
2020-08-16 16:41:59 -0500

    Fetch dino.json test and Dino Constructor

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-  //Fetching data from JSON file
-  fetch("dino.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
+      //Fetching data from JSON file
+      fetch("dino.json")
+      .then(response => response.json())
+      .then(dinos => new Array())
+      .then(dinos => console.log(dinos));
+
+      // Create Dino Constructor
+      function Dino(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+      }
+
+      // Create Dino Objects
+
+
+      // Create Human Object
+
+
+
+      // Use IIFE to get human data from form
+
+
+      // Create Dino Compare Method 1
+      // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+      // Create Dino Compare Method 2
+      // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+      // Create Dino Compare Method 3
+      // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
 
 
-    // Create Dino Constructor
-    function Dino() {
-
-    }
+      // Generate Tiles for each Dino in Array
 
 
-    // Create Dino Objects
-    let dino = new Dino();
+            // Add tiles to DOM
 
-
-    // Create Human Object
-    let human = new Dino();
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-
-        // Add tiles to DOM
-
-    // Remove form from screen
+      // Remove form from screen
 
 
 // On button click, prepare and display infographic
