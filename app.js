b7125b57a22b2a515c1010b5223c86281f5f5c49
jamieleegtb <cemilegtaman@gmail.com>
2020-08-15 22:39:43 -0500

    Fetch request test

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
+  //Fetching data from JSON file
+  fetch("dino.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+
+
 
     // Create Dino Constructor
+    function Dino() {
+
+    }
 
 
     // Create Dino Objects
+    let dino = new Dino();
 
 
     // Create Human Object
+    let human = new Dino();
 
     // Use IIFE to get human data from form
 
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
