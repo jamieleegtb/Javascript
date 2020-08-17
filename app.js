e4b2f243e96278e4aeaebcee9306be6893d59d8e
jamieleegtb <cemilegtaman@gmail.com>
2020-08-17 14:32:43 -0500

    Fetch JSON and generate tiles test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
       document.body.onload = generateDinos;
 
       // Create Dino Constructor
-      function Dino(species, weight, height, diet, where, when, fact, image) {
+      function Dino({species, weight, height, diet, where, when, fact, image}) {
         this.species = species;
         this.weight = weight;
         this.height = height;
@@ -13,94 +13,6 @@
       }
 
       // Create Dino Objects
-      // After post the project I'll focus on fetching the JSON data
-      /*const triceratops = new Dino(
-        "Triceratops",
-        13000,
-        114,
-        "herbavor",
-        "North America",
-        "Late Cretaceous",
-        "First discovered in 1889 by Othniel Charles Marsh",
-        "images/triceratops.png"
-      );
-
-      const tyrannosaurus = new Dino(
-        "Tyrannosaurus Rex",
-        11905,
-        144,
-        "carnivor",
-        "North America",
-        "Late Cretaceous",
-        "The largest known skull measures in at 5 feet long.",
-        "images/tyrannosaurus rex.png"
-      );
-
-      const anklyosaurus = new Dino(
-        "Anklyosaurus",
-        10500,
-        55,
-        "herbavor",
-        "North America",
-        "Late Cretaceous",
-        "Anklyosaurus survived for approximately 135 million years.",
-        "images/anklyosaurus.png"
-      );
-
-      const brachiosaurus = new Dino(
-        "Brachiosaurus",
-        70000,
-        "372",
-        "herbavor",
-        "North America",
-        "Late Jurasic",
-        "An asteroid was named 9954 Brachiosaurus in 1991.",
-        "images/brachiosaurus.png"
-      );
-
-      const stegosaurus = new Dino(
-        "Stegosaurus",
-        11600,
-        79,
-        "herbavor",
-        "North America, Europe, Asia",
-        "Late Jurasic to Early Cretaceous",
-        "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
-        "images/stegosaurus.png"
-      );
-
-      const elasmosaurus = new Dino(
-        "Elasmosaurus",
-        16000,
-        59,
-        "carnivor",
-        "North America",
-        "Late Cretaceous",
-        "Elasmosaurus was a marine reptile first discovered in Kansas.",
-        "images/elasmosaurus.png"
-      );
-
-      const pteranodon = new Dino(
-        "Pteranodon",
-        44,
-        20,
-        "carnivor",
-        "North America",
-        "Late Cretaceous",
-        "Actually a flying reptile, the Pteranodon is not a dinosaur.",
-        "images/pteranodon.png"
-      );
-
-      const pigeon = new Dino(
-        "Pigeon",
-        0.5,
-        9,
-        "herbavor",
-        "World Wide",
-        "Holocene",
-        "All birds are living dinosaurs.",
-        "images/pigeon.png"
-      );*/
 
       // Fetch Dino from dino.json file
       let dinos = [];
@@ -167,9 +79,11 @@
         }
       }
 
+
       // Generate Tiles for each Dino in Array
       function generateTiles (){
         const grid = document.querySelector("#grid");
+        // Add tiles to DOM
         grid.innerHTML += `
               <div class="grid-item">
                   <h3>${dinos.species}</h3>
@@ -180,7 +94,7 @@
             `;
         };
 
-        // Add tiles to DOM
+
 
       // Remove form from screen
       function removeForm() {
