87e472122c5482b559cc627adffcd0dd5f590ee6
jamieleegtb <cemilegtaman@gmail.com>
2020-08-16 21:18:20 -0500

    Deleted the fetch and moved the JSON data to the JS file

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-      //Fetching data from JSON file
-      (function(){
-        fetch("dino.json")
-          .then(response => response.json())
-          .then(dino => console.log(dino));
-      })();
-
 
       // Create Dino Constructor
       function Dino(species, weight, height, diet, where, when, fact) {
@@ -18,7 +11,80 @@
       }
 
       // Create Dino Objects
-      const
+      // After post the project I'll focus on fetching the JSON data
+      const triceratops = new Dino(
+        "Triceratops",
+        13000,
+        114,
+        "herbavor",
+        "North America",
+        "Late Cretaceous",
+        "First discovered in 1889 by Othniel Charles Marsh"
+      );
+
+      const tyrannosaurus = new Dino(
+        "Tyrannosaurus Rex",
+        11905,
+        144,
+        "carnivor",
+        "North America",
+        "Late Cretaceous",
+        "The largest known skull measures in at 5 feet long."
+      );
+      const anklyosaurus = new Dino(
+        "Anklyosaurus",
+        10500,
+        55,
+        "herbavor",
+        "North America",
+        "Late Cretaceous",
+        "Anklyosaurus survived for approximately 135 million years."
+      );
+      const brachiosaurus = new Dino(
+        "Brachiosaurus",
+        70000,
+        "372",
+        "herbavor",
+        "North America",
+        "Late Jurasic",
+        "An asteroid was named 9954 Brachiosaurus in 1991."
+      );
+      const stegosaurus = new Dino(
+        "Stegosaurus",
+        11600,
+        79,
+        "herbavor",
+        "North America, Europe, Asia",
+        "Late Jurasic to Early Cretaceous",
+        "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+      );
+      const elasmosaurus = new Dino(
+        "Elasmosaurus",
+        16000,
+        59,
+        "carnivor",
+        "North America",
+        "Late Cretaceous",
+        "Elasmosaurus was a marine reptile first discovered in Kansas."
+      );
+      const pteranodon = new Dino(
+        "Pteranodon",
+        44,
+        20,
+        "carnivor",
+        "North America",
+        "Late Cretaceous",
+        "Actually a flying reptile, the Pteranodon is not a dinosaur."
+      );
+      const pigeon = new Dino(
+        "Pigeon",
+        0.5,
+        9,
+        "herbavor",
+        "World Wide",
+        "Holocene",
+        "All birds are living dinosaurs."
+      );
 
 
       // Create Human Object
