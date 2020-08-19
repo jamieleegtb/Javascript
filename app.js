13ba654dcb7ec7367c99259264039828ee92e79c
jamieleegtb <cemilegtaman@gmail.com>
2020-08-18 23:19:05 -0500

    To fix fetch API error locally moved the dino objects to JS file

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 
-      // Load Dinosaurs
-      document.body.onload = generateDinos;
 
       // Create Dino Constructor
-      function Dino({species, weight, height, diet, where, when, fact, image}) {
+      function Dino(species, weight, height, diet, where, when, fact, image) {
         this.species = species;
         this.weight = weight;
         this.height = height;
@@ -14,16 +12,99 @@
       }
 
       // Create Dino Objects
-      // Fetch Dino from dino.json file
-      let dinos = [];
 
-       async function generateDinos(){
-         const myJson = await fetch("dino.json");
-         const data = await myJson.json();
+      const triceratops = new Dino(
+        "Triceratops",
+        13000,
+        114,
+        "herbavor",
+        "North America",
+        "Late Cretaceous",
+        "First discovered in 1889 by Othniel Charles Marsh"
+      );
 
-         // Generate Dino objects array
-         dinos = data.Dinos.map(dino => new Dino(dino));
-       }
+      const tyrannosaurus = new Dino(
+        "Tyrannosaurus Rex",
+        11905,
+        144,
+        "carnivor",
+        "North America",
+        "Late Cretaceous",
+        "The largest known skull measures in at 5 feet long."
+      );
+
+      const anklyosaurus = new Dino(
+        "Anklyosaurus",
+        10500,
+        55,
+        "herbavor",
+        "North America",
+        "Late Cretaceous",
+        "Anklyosaurus survived for approximately 135 million years."
+      );
+
+      const brachiosaurus = new Dino(
+        "Brachiosaurus",
+        70000,
+        "372",
+        "herbavor",
+        "North America",
+        "Late Jurasic",
+        "An asteroid was named 9954 Brachiosaurus in 1991."
+      );
+
+      const stegosaurus = new Dino(
+        "Stegosaurus",
+        11600,
+        79,
+        "herbavor",
+        "North America, Europe, Asia",
+        "Late Jurasic to Early Cretaceous",
+        "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+      );
+
+      const elasmosaurus = new Dino(
+        "Elasmosaurus",
+        16000,
+        59,
+        "carnivor",
+        "North America",
+        "Late Cretaceous",
+        "Elasmosaurus was a marine reptile first discovered in Kansas."
+      );
+
+      const pteranodon = new Dino(
+        "Pteranodon",
+        44,
+        20,
+        "carnivor",
+        "North America",
+        "Late Cretaceous",
+        "Actually a flying reptile, the Pteranodon is not a dinosaur."
+      );
+
+      const pigeon = new Dino(
+        "Pigeon",
+        0.5,
+        9,
+        "herbavor",
+        "World Wide",
+        "Holocene",
+        "All birds are living dinosaurs.",
+      );
+
+      // Dino array
+      let dinos =  [ triceratops,
+                     tyrannosaurus,
+                     anklyosaurus,
+                     brachiosaurus,
+                     stegosaurus,
+                     elasmosaurus,
+                     pteranodon,
+                     pigeon
+                   ];
+
+
 
       // Create Human Object
       let human = {};
@@ -87,7 +168,7 @@
           grid.innerHTML += `
             <div class="grid-item">
               <h3>${dino.species}</h3>
-              <img src="./images/${dino.species.toLowerCase()}.png" alt="${dino.species}" />
+              <img src="./images/${dino.species}.png" alt="${dino.species}" />
               <p>${dino.fact}</p>
             </div>
           `;
@@ -103,8 +184,6 @@
             <img src="./images/human.png">
           </div>
         `);
-
-
       };
 
 
